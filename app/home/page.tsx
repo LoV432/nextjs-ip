c567b3d39d541fd721b2b7415e3443538a582ea0
LoV432 <monib619@gmail.com>
2023-05-08 19:22:14 +0500

    add /home page

---
+++ app/home/page.tsx
@@ -0,0 +1,15 @@
+import { headers } from 'next/headers';
+import style from './home.module.css';
+const ipHeader = process.env['IP_HEADER'] || 'X-Forwarded-For';
+
+export default function Home() {
+	let ip = headers().get(ipHeader) || 'unknow';
+	const isipv6 = ip.includes(':') ? true : false;
+	return (
+		<div className="grid w-screen h-screen place-items-center bg-zinc-900">
+			<h1 id={style.homeText} className={`${isipv6 ? 'text-justify text-[7vw]' : 'text-justify text-[11vw] md:text-[9vw]'} font-bold text-white p-2`}>
+				{ip}
+			</h1>
+		</div>
+	);
+}
