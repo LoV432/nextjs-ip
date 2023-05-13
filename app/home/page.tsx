8b1fdc1f38aef3ae5524f7976b51ff046988f2f3
LoV432 <monib619@gmail.com>
2023-05-13 16:42:21 +0500

    fetch ip from / so we can cache /home

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,16 +1,13 @@
-import { headers } from 'next/headers';
 import Ip from './ip.client';
 import type { Metadata } from 'next';
-const ipHeader = process.env['IP_HEADER'] || 'X-Forwarded-For';
 export const metadata: Metadata = {
 	title: 'Vomit IP'
 };
 
 export default function Home() {
-	let ip = headers().get(ipHeader) || 'unknown';
 	return (
 		<div className="grid h-screen w-screen place-items-center bg-zinc-900">
-			<Ip ip={ip} />
+			<Ip />
 		</div>
 	);
 }
