9a48ba4a64a05a6ddab6fbb5c33519accf642a65
LoV432 <monib619@gmail.com>
2023-05-14 21:02:59 +0500

    make /home cache-able

--- app/home/ip.client.tsx
+++ app/home/ip.client.tsx
@@ -1,11 +1,11 @@
 'use client';
 import style from './home.module.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-export default function Ip({ ip }: { ip: string }) {
+export default function Ip() {
 	const [clipboardAnimation, setClipboardAnimation] = useState(style.clipboardHidden);
-
-	const isipv6 = ip.includes(':') ? true : false;
+	const [ip, setIp] = useState('');
+	const [isipv6, setIsipv6] = useState(false);
 	function copyIp() {
 		navigator.clipboard.writeText(ip);
 		if (clipboardAnimation === style.clipboardAnimation) return; // Dont re-run animation if its alrdy running
@@ -14,6 +14,21 @@ export default function Ip({ ip }: { ip: string }) {
 			setClipboardAnimation(style.clipboardHidden);
 		}, 2350);
 	}
+	function randomIp() {
+		return `${Math.floor(Math.random() * 255)}.${Math.floor(Math.random() * 255)}.${Math.floor(Math.random() * 255)}.${Math.floor(Math.random() * 255)}`;
+	}
+	useEffect(() => {
+		const interval = setInterval(() => setIp(randomIp()), 100); // Keep setting random IPs until we get real ip
+		(async () => {
+			const requestIp = await fetch('/');
+			clearInterval(interval); // We have the real IP here so stop interval
+			await new Promise((resolve) => setTimeout(resolve, 100)); // Makes the switch to real IP more consistent-ish
+			const realIp = await requestIp.text();
+			setIsipv6(realIp.includes(':') ? true : false);
+			setIp(realIp);
+		})();
+		return () => clearInterval(interval);
+	}, []);
 	return (
 		<>
 			<h1 onClick={copyIp} className={`${isipv6 ? 'text-[7vw]' : 'text-[11vw] md:text-[9vw]'} ${style.homeText} select-none p-2 text-justify font-bold text-white hover:cursor-pointer`}>
