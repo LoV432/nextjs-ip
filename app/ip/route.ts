10703f89a5bcb0da6b61b5fc3168009106d3ffdd
LoV432 <monib619@gmail.com>
2023-05-14 15:27:15 +0500

    fix build error

--- app/ip/route.ts
+++ app/ip/route.ts
@@ -2,8 +2,8 @@ import { NextRequest, NextResponse } from 'next/server';
 const ipHeader = process.env['IP_HEADER'] || 'X-Forwarded-For';
 export const runtime = 'edge';
 
-export default (req: NextRequest, res: NextResponse) => {
+export function GET(req: NextRequest, res: NextResponse) {
 	const ip = req.headers.get(ipHeader);
 	res.headers.set('cache-control', 'no-store');
 	return new Response(ip);
-};
+}
