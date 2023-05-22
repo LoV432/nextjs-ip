fd4d6da1e10d088fc2e00d2fa24e7d98d106d446
LoV432 <monib619@gmail.com>
2023-05-23 00:45:39 +0500

    include ip in /geo

--- app/geo/route.ts
+++ app/geo/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse, type NextRequest } from 'next/server';
 export const runtime = 'edge';
+const ipHeader = process.env['IP_HEADER'] || '';
 const continentHeader = process.env['CONTINENT_HEADER'] || '';
 const countryHeader = process.env['COUNTRY_HEADER'] || '';
 const stateHeader = process.env['STATE_HEADER'] || '';
@@ -8,13 +9,14 @@ const longitudeHeader = process.env['LATITUDE_HEADER'] || '';
 const latitudeHeader = process.env['LONGITUDE_HEADER'] || '';
 
 export async function GET(req: NextRequest) {
+	const ip = getValueFromHeader(ipHeader, req);
 	const continent = getValueFromHeader(continentHeader, req);
 	const country = getValueFromHeader(countryHeader, req);
 	const state = getValueFromHeader(stateHeader, req);
 	const city = getValueFromHeader(cityHeader, req);
 	const longitude = getValueFromHeader(longitudeHeader, req);
 	const latitude = getValueFromHeader(latitudeHeader, req);
-	return NextResponse.json({ continent, country, state, city, longitude, latitude });
+	return NextResponse.json({ ip, continent, country, state, city, longitude, latitude });
 }
 
 function getValueFromHeader(header: string, req: NextRequest) {
