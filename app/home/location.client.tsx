ad4baac6f8d246366ab9c4ea5d416ed81a29cb45
LoV432 <monib619@gmail.com>
2023-05-24 19:48:25 +0500

    rgb location text

--- app/home/location.client.tsx
+++ app/home/location.client.tsx
@@ -1,11 +1,12 @@
+import style from './home.module.css';
 export default function Location({ location }: { location: { country: string; city: string } }) {
 	let regionNames = new Intl.DisplayNames(['en'], { type: 'region' });
 	const country = location.country === 'Unknown' ? '-' : regionNames.of(location.country);
 	const city = location.city === 'Unknown' ? '-' : location.city;
 	return (
 		<>
-			<h1 className={`select-none text-justify text-6xl font-bold`}>{location.country}</h1>
-			<h1 className={`select-none text-justify text-6xl font-bold`}>{location.city}</h1>
+			<h1 className={`${country === '-' ? '' : style.countryText} text-justify text-6xl font-bold`}>{country}</h1>
+			<h1 className={`${city === '-' ? '' : style.cityText} text-justify text-6xl font-bold`}>{city}</h1>
 		</>
 	);
 }
