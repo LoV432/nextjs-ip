41ef5fdf6c79a1143e55094c1272f5783b821a85
LoV432 <monib619@gmail.com>
2023-05-25 20:33:27 +0500

    always fit location text

--- app/home/location.client.tsx
+++ app/home/location.client.tsx
@@ -1,12 +1,26 @@
+import { MutableRefObject, useEffect, useRef, useState } from 'react';
 import style from './home.module.css';
 export default function Location({ location }: { location: { country: string; city: string } }) {
+	const [textSize, setTextSize] = useState(60);
+	const textContainer = useRef(null) as unknown as MutableRefObject<HTMLDivElement>;
+
+	useEffect(() => {
+		if (textContainer.current.scrollWidth > textContainer.current.clientWidth) {
+			setTextSize(textSize - 1);
+		}
+	}, [textSize]);
+
 	let regionNames = new Intl.DisplayNames(['en'], { type: 'region' });
 	const country = location.country === 'Unknown' ? '-' : regionNames.of(location.country);
 	const city = location.city === 'Unknown' ? '-' : location.city;
 	return (
-		<>
-			<h1 className={`${country === '-' ? '' : style.countryText} rounded-xl border-b-2 border-solid border-zinc-800 border-opacity-40 px-4 text-justify text-[3.75rem] font-bold`}>{country}</h1>
-			<h1 className={`${city === '-' ? '' : style.cityText} mb-7 rounded-xl border-b-2 border-solid border-zinc-800 border-opacity-40 px-4 text-justify text-[3.75rem] font-bold`}>{city}</h1>
-		</>
+		<div className={`flex w-screen flex-col items-center overflow-hidden px-4 text-center md:px-0`} ref={textContainer}>
+			<h1 id="countryName" style={{ fontSize: textSize }} className={`${country === '-' ? '' : style.countryText} truncate rounded-xl border-b-2 border-solid border-zinc-800 border-opacity-40 px-4 font-bold tracking-widest`}>
+				{country}
+			</h1>
+			<h1 style={{ fontSize: textSize }} className={`${city === '-' ? '' : style.cityText} mb-7 w-fit rounded-xl border-b-2 border-solid border-zinc-800 border-opacity-40 px-4 font-bold tracking-widest`}>
+				{city}
+			</h1>
+		</div>
 	);
 }
