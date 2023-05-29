3a3a4445e92d44167a6a56be62809f6a5b4b77da
LoV432 <monib619@gmail.com>
2023-05-29 12:16:35 +0500

    only show scroll indicator when ip loaded

--- app/home/ip.client.tsx
+++ app/home/ip.client.tsx
@@ -41,7 +41,7 @@ export default function Ip({ ip }: { ip: string }) {
 				<h1 onClick={copyIp} className={`${isipv6 ? 'text-[7vw]' : 'text-[11vw] md:text-[9vw]'} ${style.homeText} select-none p-2 text-justify font-bold text-white hover:cursor-pointer`}>
 					{finalIp}
 				</h1>
-				<svg onClick={scrollToBottom} className={`${style.scrollDownAnimation} h-8 w-8 rounded-full fill-rose-900 hover:cursor-pointer`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+				<svg onClick={scrollToBottom} className={`${style.scrollDownAnimation} ${finalIp === 'Detecting...' || finalIp === '' ? 'hidden' : ''} h-8 w-8 rounded-full fill-rose-900 hover:cursor-pointer`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
 					<path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
 				</svg>
 			</div>
