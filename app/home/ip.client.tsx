f25746648e4853dae17af188b97e2aeca9e8a094
LoV432 <monib619@gmail.com>
2023-05-29 12:12:20 +0500

    add scroll indicator

--- app/home/ip.client.tsx
+++ app/home/ip.client.tsx
@@ -14,6 +14,12 @@ export default function Ip({ ip }: { ip: string }) {
 			setClipboardAnimation(style.clipboardHidden);
 		}, 2350);
 	}
+	function scrollToBottom() {
+		window.scrollTo({
+			top: document.body.scrollHeight,
+			behavior: 'smooth'
+		});
+	}
 	useEffect(() => {
 		if (ip === '') {
 			const urlParams = new URLSearchParams(window.location.search);
@@ -31,9 +37,14 @@ export default function Ip({ ip }: { ip: string }) {
 	}, [ip]);
 	return (
 		<>
-			<h1 onClick={copyIp} className={`${isipv6 ? 'text-[7vw]' : 'text-[11vw] md:text-[9vw]'} ${style.homeText} select-none p-2 text-justify font-bold text-white hover:cursor-pointer`}>
-				{finalIp}
-			</h1>
+			<div className="flex flex-col items-center gap-20">
+				<h1 onClick={copyIp} className={`${isipv6 ? 'text-[7vw]' : 'text-[11vw] md:text-[9vw]'} ${style.homeText} select-none p-2 text-justify font-bold text-white hover:cursor-pointer`}>
+					{finalIp}
+				</h1>
+				<svg onClick={scrollToBottom} className={`${style.scrollDownAnimation} h-8 w-8 rounded-full fill-rose-900 hover:cursor-pointer`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+					<path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
+				</svg>
+			</div>
 			<div className={`${style.clipboard} fixed bottom-14 rounded-lg p-3 text-lg font-semibold text-black ${clipboardAnimation}`}>Copied to clipboard!</div>
 		</>
 	);
