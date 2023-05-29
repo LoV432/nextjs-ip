b992a82d6d22af6a364a8117d0652721bcb1c0a8
LoV432 <monib619@gmail.com>
2023-05-30 00:14:16 +0500

    add gradient to arrow down icon

--- app/home/ip.client.tsx
+++ app/home/ip.client.tsx
@@ -41,11 +41,16 @@ export default function Ip({ ip }: { ip: string }) {
 				<h1 onClick={copyIp} className={`${isipv6 ? 'text-[7vw]' : 'text-[11vw] md:text-[9vw]'} ${style.homeText} select-none p-2 text-justify font-bold text-white hover:cursor-pointer`}>
 					{finalIp}
 				</h1>
-				<svg onClick={scrollToBottom} className={`${style.scrollDownAnimation} ${finalIp === 'Detecting...' || finalIp === '' ? 'hidden' : ''} h-8 w-8 rounded-full fill-rose-900 hover:cursor-pointer`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
-					<path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
-				</svg>
+				<div onClick={scrollToBottom} className={`${style.scrollDownAnimation} ${finalIp === 'Detecting...' || finalIp === '' ? 'hidden' : ''} h-16 w-16 rounded-full hover:cursor-pointer`}></div>
 			</div>
 			<div className={`${style.clipboard} fixed bottom-14 rounded-lg p-3 text-lg font-semibold text-black ${clipboardAnimation}`}>Copied to clipboard!</div>
+			<svg className="absolute h-0 w-0" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+				<g>
+					<clipPath id="arrowDown">
+						<path className={'translate-x-4 scale-[0.08]'} fill="#EC008C" d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
+					</clipPath>
+				</g>
+			</svg>
 		</>
 	);
 }
